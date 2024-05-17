2c28515f980706da5df55237fa6d1f96f150d7a5
Jörg Sommrey <28217714+jo-37@users.noreply.github.com>
2024-05-17 20:48:33 +0200

    bump to 0.975

--- lib/List/Gen.pm
+++ lib/List/Gen.pm
@@ -87,7 +87,7 @@ package List::Gen;
 
     my $MAX_IDX = eval {require POSIX; POSIX::DBL_MAX()} || 2**53 - 1;
 
-    our $VERSION = '0.974';
+    our $VERSION = '0.975';
 
 =head1 NAME
 
@@ -95,7 +95,7 @@ List::Gen - provides functions for generating lists
 
 =head1 VERSION
 
-version 0.974
+version 0.975
 
 =head1 SYNOPSIS
 
